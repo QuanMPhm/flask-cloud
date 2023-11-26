36bc252af14228a62f273dae4e6457a8510278f0
Quan Pham <quanmp@bu.edu>
2023-11-26 14:10:46 -0500

    Edit k8 test

--- tests/test_k8.py
+++ tests/test_k8.py
@@ -8,9 +8,4 @@ def test_k8():
     assert "Fizz" not in res.text
 
     res = requests.get(f"{url}/?start=5&end=5")
-    assert "Buzz" in res.text
-
-
-# # os.environ['NO_PROXY'] = '127.0.0.1'
-# res = requests.get("http://localhost:5000")
-# print(res.text)
+    assert "Buzz" not in res.text
